2aa880bfbf28359dee8c887fd8a29c42f21072f0
dopamane <dwc1295@gmail.com>
2022-01-27 03:10:38 -0800

    add process functions, docs

--- src/Jade/Process.hs
+++ src/Jade/Process.hs
@@ -1,47 +1,78 @@
+{-|
+Module      : Jade.Process
+Description : Smt process handling
+Copyright   : (c) David Cox 2022
+License     : BSD-3-Clause
+Maintainer  : dwc1295@gmail.com
+-}
 module Jade.Process (
-    SmtHandle (..),
-    withSmtProcess,
+  SmtHandle (..),
+  withSmtProcess,
+  createSmtProcess,
+  cleanupSmtProcess
 ) where
 
-import System.IO (
-    Handle,
- )
+import System.IO (Handle)
 import System.Process (
-    CreateProcess (std_err, std_in, std_out),
-    ProcessHandle,
-    StdStream (CreatePipe),
-    proc,
-    withCreateProcess,
- )
+  CreateProcess (std_err, std_in, std_out),
+  ProcessHandle,
+  StdStream (CreatePipe),
+  cleanupProcess,
+  createProcess,
+  proc,
+  withCreateProcess
+  )
 
+-- | Smt 'Handle'
 data SmtHandle = SmtHandle
-    { smtIn :: !Handle
-    , smtOut :: !Handle
-    , smtErr :: !Handle
-    , smtProc :: !ProcessHandle
-    }
+  { smtIn   :: !Handle -- ^ Input
+  , smtOut  :: !Handle -- ^ Output
+  , smtErr  :: !Handle -- ^ Error
+  , smtProc :: !ProcessHandle
+  }
 
-withSmtProcess ::
-    -- | Executable
-    FilePath ->
-    -- | Arguments
-    [String] ->
-    -- | SMT Action
-    (SmtHandle -> IO a) ->
-    IO a
-withSmtProcess exe opts action = withCreateProcess smtProcess $
-    \(Just hIn) (Just hOut) (Just hErr) hProc ->
-        action
-            SmtHandle
-                { smtIn = hIn
-                , smtOut = hOut
-                , smtErr = hErr
-                , smtProc = hProc
-                }
+-- | Smt 'CreateProcess'
+smtProcess 
+  :: FilePath -- ^ Executable
+  -> [String] -- ^ Arguments
+  -> CreateProcess
+smtProcess exe opts = (proc exe opts){ std_in  = CreatePipe
+                                     , std_out = CreatePipe
+                                     , std_err = CreatePipe
+                                     }
+
+-- | Recommended function for creation and cleanup of smt process handle.
+withSmtProcess 
+  :: FilePath            -- ^ Executable
+  -> [String]            -- ^ Arguments
+  -> (SmtHandle -> IO a) -- ^ Smt action
+  -> IO a
+withSmtProcess exe opts action = withCreateProcess (smtProcess exe opts) $
+  \(Just hIn) (Just hOut) (Just hErr) hProc -> 
+    action SmtHandle{ smtIn   = hIn
+                    , smtOut  = hOut
+                    , smtErr  = hErr
+                    , smtProc = hProc
+                    }
+
+-- | Create smt process handle. See 'withSmtProcess'.
+createSmtProcess
+  :: FilePath -- ^ Executable
+  -> [String] -- ^ Arguments
+  -> IO SmtHandle
+createSmtProcess exe opts = do
+  (Just hIn, Just hOut, Just hErr, hProc) <- createProcess $ smtProcess exe opts
+  return SmtHandle{ smtIn   = hIn
+                  , smtOut  = hOut
+                  , smtErr  = hErr
+                  , smtProc = hProc
+                  }
+
+-- | Cleanup smt process handle. See 'withSmtProcess'.
+cleanupSmtProcess :: SmtHandle -> IO ()
+cleanupSmtProcess hndl = cleanupProcess (Just hIn, Just hOut, Just hErr, hProc)
   where
-    smtProcess =
-        (proc exe opts)
-            { std_in = CreatePipe
-            , std_out = CreatePipe
-            , std_err = CreatePipe
-            }
+    hIn   = smtIn   hndl
+    hOut  = smtOut  hndl
+    hErr  = smtErr  hndl
+    hProc = smtProc hndl
