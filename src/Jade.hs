16957dd3cf9fa2813ce710729dc77acd67a57b4a
dopamane <dwc1295@gmail.com>
2022-01-27 04:18:59 -0800

    cleanup, transmit commands and general responses

--- src/Jade.hs
+++ src/Jade.hs
@@ -1,46 +1,54 @@
 module Jade (
-    -- module Jade.Command,
 
-    -- * Process
-    module Jade.Process,
-    -- module Jade.Expression,
+  -- * Process
+  module Jade.Process,
+
+  -- * Transmit
+  transmit,
+  transmit_,
+
+  -- ** Low-level interface
+  send,
+  recv,
 
     -- * Syntax
-    module Jade.Syntax,
-    -- transmit,
-    -- transmit_,
+  module Jade.Syntax
+
 ) where
 
+import Control.Monad (forM)
 import Data.Text (Text)
-import qualified Data.Text as T
+import qualified Data.Text    as T
 import qualified Data.Text.IO as TIO
 
--- import Jade.Command
--- import Jade.Expression
 import Jade.Process
 import Jade.Syntax
 
-{-
-transmit :: SmtHandle -> [Command] -> IO [Text]
-transmit hndl = mapM (\cmd -> send hndl cmd >> recv hndl)
+-- | Send 'Command's and receive 'GeneralResponse's
+transmit :: SmtHandle -> [Command] -> IO [GeneralResponse]
+transmit hndl cmds = forM cmds $ \cmd -> do
+  send hndl $ unparseCommand cmd
+  parseGeneralResponse <$> recv hndl
 
+-- | Send 'Command's without receiving 'GeneralResponse's.
 transmit_ :: SmtHandle -> [Command] -> IO ()
-transmit_ hndl = mapM_ (send hndl)
+transmit_ hndl = mapM_ (send hndl . unparseCommand)
 
-send :: SmtHandle -> Command -> IO ()
-send hndl = TIO.hPutStrLn (smtIn hndl) . express
+-- | Send 'Text' to 'SmtHandle'. See 'transmit' and 'transmit_'.
+send :: SmtHandle -> Text -> IO ()
+send hndl = TIO.hPutStrLn (smtIn hndl)
 
+-- | Receive 'Text' from 'SmtHandle'. See 'transmit'.
 recv :: SmtHandle -> IO Text
 recv hndl = T.unlines <$> readResponse 0
   where
     readResponse :: Int -> IO [Text]
     readResponse open = do
-        -- always read at least one line
-        line <- TIO.hGetLine $ smtOut hndl
-        let deltaOpen = T.count "(" line - T.count ")" line
-            totalOpen = open + deltaOpen
-        -- if all parentheses closed, return line
-        if totalOpen == 0
-            then return [line]
-            else (line :) <$> readResponse totalOpen
--}
+      -- always read at least one line
+      line <- TIO.hGetLine $ smtOut hndl
+      let deltaOpen = T.count "(" line - T.count ")" line
+          totalOpen = open + deltaOpen
+      -- if all parentheses closed, return line
+      if totalOpen == 0
+        then return [line]
+        else (line :) <$> readResponse totalOpen
