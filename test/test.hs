d0e9d0c5ee08c3817cc9df998f619f5dbdfd4556
dopamane <dwc1295@gmail.com>
2022-02-13 15:45:28 -0800

    traverse benchmarks

--- test/test.hs
+++ test/test.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 
-import Control.Monad (unless)
+import Control.Monad (forM, unless)
 import Data.Attoparsec.ByteString.Char8(IResult(Done))
 import qualified Data.ByteString.Builder as B (toLazyByteString)
 import qualified Data.ByteString.Lazy.Char8 as C (unpack)
@@ -14,8 +14,10 @@ import Subpar.Extra.Monad (
   runScript,
   runZ3
   )
-import System.Directory (listDirectory, makeAbsolute)
+import System.Directory (listDirectory)
 import System.Exit (exitFailure)
+import System.FilePath ((</>))
+import System.Posix.Files (isDirectory, getFileStatus)
 import System.Process (CreateProcess, readCreateProcess, shell)
 import Test.HUnit (Test(..), assertEqual, runTestTTAndExit)
 
@@ -25,24 +27,16 @@ main = do
   result <- syntaxTests
   unless result exitFailure
 
-  runTestTTAndExit . TestList . map testBench =<< benchFiles
+  runTestTTAndExit . TestList . map testBench =<< traverseDirs benchFiles
 
-benchFiles :: IO [FilePath]
-benchFiles = do
-  qfLiaCheck <- listDirectory =<< makeAbsolute "SMT-LIB-benchmarks/QF_LIA/check"
-  calypto    <- listDirectory =<< makeAbsolute "SMT-LIB-benchmarks/QF_LIA/calypto"
-  mapM makeAbsolute $ map ("SMT-LIB-benchmarks/QF_LIA/" <>) $
-    map ("check/" <>) qfLiaCheck
-      ++ map ("calypto/" <>) calypto
+benchFiles :: [FilePath]
+benchFiles =
+  [ "SMT-LIB-benchmarks/QF_LIA/check/"
+  , "SMT-LIB-benchmarks/QF_LIA/calypto/"
+  , "SMT-LIB-benchmarks/QF_LIA/pidgeons/"
+  ]
 {-
   [ -- "SMT-LIB-benchmarks/QF_LIA/20210219-Dartagnan/ConcurrencySafety-Main/45_monabsex1_vs-O0.smt2"
-    "SMT-LIB-benchmarks/QF_LIA/calypto/problem-001542.cvc.1.smt2"
-  ] ++ 
-    "SMT-LIB-benchmarks/QF_LIA/check/bignum_lia1.smt2"
-  , "SMT-LIB-benchmarks/QF_LIA/check/bignum_lia2.smt2"
-  , "SMT-LIB-benchmarks/QF_LIA/check/int_incompleteness1.smt2"
-  , "SMT-LIB-benchmarks/QF_LIA/check/int_incompleteness2.smt2"
-  , "SMT-LIB-benchmarks/QF_LIA/check/int_incompleteness3.smt2"
   , "SMT-LIB-benchmarks/QF_LIA/convert/convert-jpg2gif-query-1139.smt2"
   , "SMT-LIB-benchmarks/QF_LIA/cut_lemmas/10-vars/cut_lemma_01_001.smt2"
   , "SMT-LIB-benchmarks/QF_LIA/mathsat/FISCHER1-1-fair.smt2"
@@ -54,11 +48,10 @@ testBench file = TestCase $ do
   actual   <- z3Subpar file
   assertEqual "shell == subpar" expected actual
 
-z3Process :: FilePath -> CreateProcess
-z3Process file = shell $ "z3 -smt2 " ++ file
-
 z3Shell :: FilePath -> IO String
-z3Shell file = readCreateProcess (z3Process file) "" 
+z3Shell file = readCreateProcess z3Process ""
+  where
+    z3Process = shell $ "z3 -smt2 " ++ file
 
 z3Subpar :: FilePath -> IO [String]
 z3Subpar file = do
@@ -67,3 +60,21 @@ z3Subpar file = do
     Done _ response ->
       C.unpack $ B.toLazyByteString $ unparseGeneralResponse response
     _ -> error "Could not parse response."
+
+-- | [StackOverflow](https://stackoverflow.com/a/23822913/4051020)
+traverseDirs :: [FilePath] -> IO [FilePath]
+traverseDirs tops = fmap concat $ forM tops $ \top -> do
+  s <- getFileStatus top
+  if isDirectory s
+    then go top
+    else return [top]
+  where
+    go dir = do
+      ds <- listDirectory dir
+      paths <- forM ds $ \d -> do
+        let path = dir </> d
+        s <- getFileStatus path
+        if isDirectory s
+          then go path
+          else return [path]
+      return $ concat paths
